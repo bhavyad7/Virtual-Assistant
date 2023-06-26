41e5abb5940dd9ae942a5723dcc65651261c7aa1
bhavyad7 <60531046+bhavyad7@users.noreply.github.com>
2023-06-26 10:50:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+#Created by Bhavya Doshi: https://www.linkedin.com/in/bhavyadoshi7
+#Check README.md to configure the initial packages 
+
 import speech_recognition as sr
 import pyttsx3
 import pywhatkit
@@ -11,7 +14,7 @@ def talk(text):
     engine.say(text)
     engine.runAndWait()
 
-talk('hello my name is bhavya, how can i help you?')
+talk('hello my name is bhavya, how can i help you?') #Cange the name from Bhavya to your name is you like
 
 def take_command():
     with sr.Microphone() as source:
@@ -25,21 +28,21 @@ def take_command():
 
 def run_bhavya():
     command = take_command()
-    if 'play' in command:
+    if 'play' in command:  #Play from Youtube
         word_yt = command.replace('play', '')
         talk('playing' +word_yt)
         print('playing' +word_yt)
         pywhatkit.playonyt(word_yt)
-    elif 'time' in command:
+    elif 'time' in command:  #Show Time
         time = datetime.datetime.now().strftime('%I:%M %p')
         print('Current time is ' +time)
         talk('Current time is ' +time)
-    elif 'search' in command:
+    elif 'search' in command:  #Show from Wikipedia
         word = command.replace('search', '')
         info = wikipedia.summary(word, 3)
         print(info)
         talk(info)
-    else:
+    else:  #If the system did not listened the command
         print('Please say the command again.')
         talk('Please say the command again.')
 
